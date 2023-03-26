eac631fbabbca53dbfb024beef92d1ce80f377e4
jennie.shao <m65pjj@gmail.com>
2023-03-27 00:39:00 +0800

    refactor: extract KeyVision class

---
+++ lib/data/key_vision.dart
@@ -0,0 +1,12 @@
+class KeyVision {
+  late String url;
+
+  KeyVision({required this.url});
+}
+
+List<KeyVision> kvList = List.filled(
+  5,
+  KeyVision(
+      url:
+          'https://images.unsplash.com/photo-1523496922380-91d5afba98a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80'),
+);
