bc5f9f6a024aeda653f35f3c7cd3ebc4292c8adb
jennie.shao <m65pjj@gmail.com>
2023-04-09 23:11:49 +0800

    feat: setup counter

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Counter());
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BLoC'),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            '$_counter',
+            style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          IconButton(
+            onPressed: _incrementCounter,
+            icon: const Icon(Icons.add_circle),
+            iconSize: 36,
+          )
+        ]),
       ),
     );
   }
