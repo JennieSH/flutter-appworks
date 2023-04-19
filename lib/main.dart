60e8163a9dde8f970dc785ee8d120dcec843cb4a
jennie.shao <m65pjj@gmail.com>
2023-04-20 01:57:38 +0800

    feat: add page title

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'STYLiSH',
       theme: ThemeData(
         fontFamily: 'NotoSansTC',
         primarySwatch: Colors.grey,
