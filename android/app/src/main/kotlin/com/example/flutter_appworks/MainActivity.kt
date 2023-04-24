e25e02de2390b4ba5ef896af82b870d71cca216f
jennie.shao <m65pjj@gmail.com>
2023-04-24 23:10:04 +0800

    fix: convert Java syntax to Kotlin syntax

--- android/app/src/main/kotlin/com/example/flutter_appworks/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_appworks/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.flutter_appworks
 import androidx.annotation.NonNull
+
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -13,7 +14,7 @@ class MainActivity: FlutterActivity() {
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
             call, result ->
             if (call.method == "getAndroidString") {
-                String androidString = "Hello from Android"
+                val androidString = "Hello from Android"
                 result.success(androidString)
             } else {
                 result.notImplemented()
