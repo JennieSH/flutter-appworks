8076fc4a2f667ab16d26c8e86e140a0f0929e47c
jennie.shao <m65pjj@gmail.com>
2023-05-02 00:53:58 +0800

    refactor: extract to single page file(SearchLocationPage)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_appworks/location_service.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_appworks/search_location_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,109 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MapPage(),
-    );
-  }
-}
-
-class MapPage extends StatefulWidget {
-  const MapPage({super.key});
-
-  @override
-  State<MapPage> createState() => _MapPageState();
-}
-
-class _MapPageState extends State<MapPage> {
-  late GoogleMapController _mapController;
-  final TextEditingController _searchController = TextEditingController();
-  final Set<Marker> _markers = <Marker>{};
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    _mapController = controller;
-  }
-
-  void _setMarker(LatLng point) {
-    setState(() {
-      _markers.add(Marker(markerId: const MarkerId('marker'), position: point));
-    });
-  }
-
-  Future<void> _goToPlace(Map<String, dynamic> place) async {
-    final double lat = place['geometry']['location']['lat'];
-    final double lng = place['geometry']['location']['lng'];
-
-    final GoogleMapController controller = await _mapController;
-
-    controller.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: LatLng(lat, lng),
-          zoom: 12,
-        ),
-      ),
-    );
-
-    _setMarker(LatLng(lat, lng));
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _setMarker(_center);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                    child: TextFormField(
-                  controller: _searchController,
-                  decoration: const InputDecoration(
-                    hintText: 'Search by City',
-                    contentPadding: EdgeInsets.all(8),
-                  ),
-                  onFieldSubmitted: (value) async {
-                    var place = await LocationService().getPlace(value);
-
-                    _goToPlace(place);
-                  },
-                )),
-                IconButton(
-                  onPressed: () async {
-                    var place = await LocationService()
-                        .getPlace(_searchController.text);
-
-                    // print(place);
-                    _goToPlace(place);
-                  },
-                  icon: const Icon(Icons.search),
-                )
-              ],
-            ),
-            Expanded(
-              child: GoogleMap(
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: CameraPosition(
-                  target: _center,
-                  zoom: 11.0,
-                ),
-                markers: _markers,
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const SearchLocationPage(),
     );
   }
 }
---
+++ lib/search_location_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_appworks/location_service.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class SearchLocationPage extends StatefulWidget {
+  const SearchLocationPage({super.key});
+
+  @override
+  State<SearchLocationPage> createState() => _SearchLocationPageState();
+}
+
+class _SearchLocationPageState extends State<SearchLocationPage> {
+  late GoogleMapController _mapController;
+  final TextEditingController _searchController = TextEditingController();
+  final Set<Marker> _markers = <Marker>{};
+  final LatLng _center = const LatLng(25.0384799, 121.5323702);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
+  void _setMarker(LatLng point) {
+    setState(() {
+      _markers.add(Marker(markerId: const MarkerId('marker'), position: point));
+    });
+  }
+
+  Future<void> _goToPlace(Map<String, dynamic> place) async {
+    final double lat = place['geometry']['location']['lat'];
+    final double lng = place['geometry']['location']['lng'];
+
+    final GoogleMapController controller = await _mapController;
+
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(lat, lng),
+          zoom: 15,
+        ),
+      ),
+    );
+
+    _setMarker(LatLng(lat, lng));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _setMarker(_center);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    child: TextFormField(
+                  controller: _searchController,
+                  decoration: const InputDecoration(
+                    hintText: 'Search by City',
+                    contentPadding: EdgeInsets.all(8),
+                  ),
+                  onFieldSubmitted: (value) async {
+                    var place = await LocationService().getPlace(value);
+
+                    _goToPlace(place);
+                  },
+                )),
+                IconButton(
+                  onPressed: () async {
+                    var place = await LocationService()
+                        .getPlace(_searchController.text);
+
+                    // print(place);
+                    _goToPlace(place);
+                  },
+                  icon: const Icon(Icons.search),
+                )
+              ],
+            ),
+            Expanded(
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 12.0,
+                ),
+                markers: _markers,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
